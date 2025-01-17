62baf07935db012915a6065e8c6c8c10bd5f1e7a
imag-er <2274265784@qq.com>
2025-01-17 08:14:12 +0800

    feat: 添加注册中心

--- src/user/handler.go
+++ src/user/handler.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"log"
 	user "github.com/imag-er/tiktok_e-commerce/src/user/kitex_gen/user"
 )
 
@@ -11,11 +12,17 @@ type UserServiceImpl struct{}
 // Register implements the UserServiceImpl interface.
 func (s *UserServiceImpl) Register(ctx context.Context, req *user.RegisterReq) (resp *user.RegisterResp, err error) {
 	// TODO: Your code here...
-	return
+	log.Printf("User: %s register with passwd: %s, confirm passwd: %s\n", req.Email, req.Password,req.ConfirmPassword)
+	return &user.RegisterResp{
+		UserId: 123456,
+	}, nil
 }
 
 // Login implements the UserServiceImpl interface.
 func (s *UserServiceImpl) Login(ctx context.Context, req *user.LoginReq) (resp *user.LoginResp, err error) {
 	// TODO: Your code here...
-	return
+	log.Printf("User: %s login with passwd: %s\n", req.Email, req.Password)
+	return &user.LoginResp{
+		UserId: 123456,
+	}, nil
 }
--- src/user/main.go
+++ src/user/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	server "github.com/cloudwego/kitex/server"
 	user "github.com/imag-er/tiktok_e-commerce/src/user/kitex_gen/user/userservice"
+	etcd "github.com/kitex-contrib/registry-etcd"
+
 	"log"
 )
 
 func main() {
-	svr := user.NewServer(new(UserServiceImpl))
+	etcd, err := etcd.NewEtcdRegistry([]string{"127.0.0.1:2379"})
 
-	err := svr.Run()
+	svr := user.NewServer(
+		new(UserServiceImpl),
+		server.WithRegistry(etcd),
+	)
+
+	err = svr.Run()
 
 	if err != nil {
 		log.Println(err.Error())
