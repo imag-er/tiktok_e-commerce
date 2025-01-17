c929a145fb97bf89bfb4de09db6b722b24d150b4
imag-er <2274265784@qq.com>
2025-01-18 05:27:09 +0800

    feat: 容器化运行组件，成功在容器内调用rpc

--- src/api/main.go
+++ src/api/main.go
@@ -5,16 +5,18 @@ import (
 	"log"
 	"os"
 
+	"time"
+
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/app/server"
 	"github.com/cloudwego/kitex/client"
-	api "github.com/imag-er/tiktok_e-commerce/src/user/kitex_gen/user"
-	clients "github.com/imag-er/tiktok_e-commerce/src/user/kitex_gen/user/userservice"
+	"github.com/imag-er/tiktok_e-commerce/src/user/kitex_gen/user"
+	"github.com/imag-er/tiktok_e-commerce/src/user/kitex_gen/user/userservice"
 	etcd "github.com/kitex-contrib/registry-etcd"
 )
 
+var etcdAddr string
 
-var etcdAddr string 
 func main() {
 
 	// get etcd address from env
@@ -26,45 +28,47 @@ func main() {
 
 	log.Println("ETCD_ENDPOINT is set to ", etcdAddr)
 
+	r, err := etcd.NewEtcdResolver([]string{etcdAddr})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
 
 	h := server.New(
 		server.WithHostPorts(":8888"),
+	)
+
+
+	userclient := userservice.MustNewClient(
+		"user",
+		client.WithResolver(r),
 	)
 
 	h.GET("/hello", func(ctx context.Context, req *app.RequestContext) {
 		req.String(200, "Hello, World!")
 	})
 
-
 	api := h.Group("/api")
 	user := api.Group("/user")
-	user.GET("/login", loginHandler)
-	user.GET("/hello", func(ctx context.Context, req *app.RequestContext) {
-		req.JSON(200, "Hello, World!")
+	user.GET("/login", func(c context.Context, ctx *app.RequestContext) {
+		loginHandler(c, ctx, userclient)
 	})
+
 
 	h.Spin()
 }
 
-func loginHandler(ctx context.Context, req *app.RequestContext) {
-	etcd, err := etcd.NewEtcdResolver([]string{etcdAddr})
-
-	// 创建user服务的客户端
-	client, err := clients.NewClient(
-		"user",
-		client.WithResolver(etcd),
-	)
-
+func loginHandler(ctx context.Context, req *app.RequestContext, client userservice.Client) {
+	rpcreq := &user.LoginReq{Email: "rpc_call@test.com", Password: "rpc_call_passwprd"}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	resp, err := client.Login(ctx, rpcreq)
+	cancel()
 	if err != nil {
-		log.Fatal(err)
-	}
-	
-	rpcreq := &api.LoginReq{Email: "rpc_call@test.com", Password: "rpc_call_passwprd"}
-	resp, err := client.Login(context.Background(), rpcreq)
-	if err != nil {
-		log.Fatal(err)
+		log.Println("On error:", err)
+		req.JSON(500, "Internal Server Error")
+		return
 	}
 	log.Println(resp)
-
 	req.JSON(200, resp)
 }
